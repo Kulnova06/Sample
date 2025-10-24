b0ea7283806cc61656c61a0e4c554e1039ded235
Kuldeep <ksinda05@gmail.com>
2025-10-25 03:30:28 +0530

    Added feature1 by kuldeep

--- beta.java
+++ beta.java
@@ -3,5 +3,6 @@ public class{
     {
         System.out.print("I am Beta");
         System.out.println("I am Gamma");
+        System.out.println("This should only be in feature 1 branch");
     }
 }
